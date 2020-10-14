924e0508a6455a26b10f988396a4f1236a248371
nejdetkadir <nejdetkadir.550@gmail.com>
2020-10-14 19:04:26 +0300

    Add when connected event of user

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,11 +1,28 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
-  const url = 'http://localhost:3000'; // development
-  indexFactory.connectSocket(url, {
-    reconnectionAttempts: 3,
-    reconnectionDelay: 600
-  }).then((socket) => {
-    //console.log('connection successfully', socket);
-  }).catch((err) => {
-    console.log(err);
-  });
+  $scope.messages = [];
+  $scope.users = {};
+
+  $scope.init = () => {
+    const username = prompt('Please enter your username');
+    if (username)
+      initSocket(username);
+    else
+      return false;
+  };
+
+  function initSocket(username) {
+    const url = 'http://localhost:3000'; // development
+    indexFactory.connectSocket(url, {
+      reconnectionAttempts: 3,
+      reconnectionDelay: 600
+    }).then((socket) => {
+      socket.emit('newUser', {username});
+      socket.on('newUser', (data) => {
+        $scope.users[data.id] = data;
+        $scope.$apply();
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
 }]);
--- src/socketAPI.js
+++ src/socketAPI.js
@@ -1,12 +1,18 @@
 const socketio = require('socket.io');
 const io = socketio();
-const socketAPI = {
-
-};
+const socketAPI = {};
+const users = {};
 
 socketAPI.io = io;
 io.on('connection', (socket) => {
-  //console.log('a user connected.');
+  socket.on('newUser', (data) => {
+    const defaultData = {
+      id: socket.id
+    };
+    const userData = Object.assign(data, defaultData);
+    users[socket.id] = userData;
+    socket.broadcast.emit('newUser', userData);
+  });
 });
 
 module.exports = socketAPI;
