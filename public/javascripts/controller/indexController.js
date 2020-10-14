58ab3e5de5c5f93f4b04bc5f9f9c360ae2186ce9
nejdetkadir <nejdetkadir.550@gmail.com>
2020-10-14 18:28:44 +0300

    implement socket.io

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,3 +1,11 @@
-app.controller('indexController', ['$scope', ($scope) => {
-  console.log('hey');
+app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
+  const url = 'http://localhost:3000'; // development
+  indexFactory.connectSocket(url, {
+    reconnectionAttempts: 3,
+    reconnectionDelay: 600
+  }).then((socket) => {
+    //console.log('connection successfully', socket);
+  }).catch((err) => {
+    console.log(err);
+  });
 }]);
